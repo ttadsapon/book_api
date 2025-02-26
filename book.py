9c61b35e79f88ebc1eb9b52a1976e9f3495e7fd3
ttadsapon <tudphon.b@ku.th>
2025-02-26 19:25:12 +0000

    backend

--- book.py
+++ book.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+from pymongo import MongoClient
+from bson import ObjectId
 
-# Sample data (in-memory database for simplicity)
-books = [
-    {"id": 1, "title": "The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About", "author": "Mel Robbins", "image_url": "https://images-na.ssl-images-amazon.com/images/I/91I1KDnK1kL._AC_UL381_SR381,381_.jpg"},
-    {"id": 2, "title": "Forgotten Home Apothecary : 250 Powerful Remedies at Your Fingertips", "author": "Dr. Nicole Apelian", "image_url": "https://images-na.ssl-images-amazon.com/images/I/91-E86oM2IL._AC_UL381_SR381,381_.jpg"},
-    {"id": 3, "title": "Seven Things You Can't Say About China", "author": "Tom Cotton", "image_url": "https://images-na.ssl-images-amazon.com/images/I/81+mN748qkL._AC_UL381_SR381,381_.jpg"},
-    {"id": 4, "title": "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones", "author" : "James Clear", "image_url": "https://images-na.ssl-images-amazon.com/images/I/81ANaVZk5LL._AC_UL381_SR381,381_.jpg"}
-]
+# MongoDB Connection URI (replace with your credentials securely)
+uri = "mongodb+srv://tudphon:s12345@cluster0.yosw1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Connect to MongoDB
+client = MongoClient(uri)
+db = client["book"]
+collection = db["books"]
 
 app = Flask(__name__)
 CORS(app)
-app.config['CORS_HEADERS']='Content-Type'
 
 @app.route("/")
 def hello_world():
@@ -21,49 +22,67 @@ def hello_world():
 @app.route('/books', methods=['POST'])
 def create_book():
     data = request.get_json()
-
+    
     new_book = {
-        "id": len(books) + 1,
         "title": data["title"],
         "author": data["author"],
         "image_url": data["image_url"]
     }
 
-    books.append(new_book)
+    inserted = collection.insert_one(new_book)
+    new_book["_id"] = str(inserted.inserted_id)  
     return jsonify(new_book), 201
 
 # Read (GET) operation - Get all books
 @app.route('/books', methods=['GET'])
-@cross_origin()
 def get_all_books():
+    books = list(collection.find({}, {"_id": 1, "title": 1, "author": 1, "image_url": 1}))
+    for book in books:
+        book["_id"] = str(book["_id"])  
     return jsonify({"books": books})
 
 # Read (GET) operation - Get a specific book by ID
-@app.route('/books/<int:book_id>', methods=['GET'])
+@app.route('/books/<string:book_id>', methods=['GET'])
 def get_book(book_id):
-    book = next((b for b in books if b["id"] == book_id), None)
+    book = collection.find_one({"_id": ObjectId(book_id)})
     if book:
+        book["_id"] = str(book["_id"])  
         return jsonify(book)
     else:
         return jsonify({"error": "Book not found"}), 404
 
 # Update (PUT) operation
-@app.route('/books/<int:book_id>', methods=['PUT'])
+@app.route('/books/<string:book_id>', methods=['PUT'])
 def update_book(book_id):
-    book = next((b for b in books if b["id"] == book_id), None)
-    if book:
-        data = request.get_json()
-        book.update(data)
-        return jsonify(book)
+    data = request.get_json()
+    updated = collection.update_one({"_id": ObjectId(book_id)}, {"$set": data})
+    
+    if updated.matched_count > 0:
+        return jsonify({"message": "Book updated successfully"})
     else:
         return jsonify({"error": "Book not found"}), 404
-    
+
 # Delete operation
-@app.route('/books/<int:book_id>', methods=['DELETE'])
+@app.route('/books/<string:book_id>', methods=['DELETE'])
 def delete_book(book_id):
-    global books
-    books = [b for b in books if b["id"] != book_id]
-    return jsonify({"message": "Book deleted successfully"})
+    deleted = collection.delete_one({"_id": ObjectId(book_id)})
+    
+    if deleted.deleted_count > 0:
+        return jsonify({"message": "Book deleted successfully"})
+    else:
+        return jsonify({"error": "Book not found"}), 404
+
+# Check password operation
+@app.route('/check-password', methods=['POST'])
+def check_password():
+    data = request.get_json()
+    password = data.get('password')
+    correct_password = "25846"  
+
+    if password == correct_password:
+        return jsonify({"isValid": True})
+    else:
+        return jsonify({"isValid": False})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
